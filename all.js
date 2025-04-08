8ef7a2a3b2296215517e0a90fdef7c3895343ad2
DoryH <doryhung@fhnet.com.tw>
2025-04-08 16:54:12 +0800

    1st push

---
+++ all.js
@@ -0,0 +1,21 @@
+// document.getElementById('countId').onclick = function () {
+//   var humNum = document.getElementById('humNum').value;
+//   var cokeNum = document.getElementById('cokeNum').value;
+
+//   alert(humNum);
+//   alert(cokeNum);
+// };
+
+// document.getElementById('calcBtn').onclick = function () {
+//   var price = document.getElementById('priceId').value;
+//   var quantity = document.getElementById('quantityId').value;
+//   document.getElementById('resultId').textContent = '總金額'+ (price * quantity);
+// };
+
+document.getElementById('calcBtn').onclick = function () {
+    var price = parseInt(document.getElementById('priceId').value);
+    var qty = parseInt(document.getElementById('quantityId').value);
+    document.getElementById('resultId').textContent = '總金額：' + (price * qty);
+    
+    alert('總金額：' + (price * qty));
+};
